0b98e804ed509abb84c786b504088498fe25cfc3
Humberto Elizondo <heb.humberto@live.com>
2022-09-17 19:20:35 -0700

    MigrationChanges

---
+++ Migrations/20220918020811_AcceptingFromHElizondoB2022.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmallStructuresTakeOffs.Migrations
+{
+    public partial class AcceptingFromHElizondoB2022 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
